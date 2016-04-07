d687a6e0b7ea93c08aad7e767ce3b3ae6f67ae11
iAmNathanJ <jacobs@heysparkbox.com>
2016-04-07 13:45:09 -0400

    update life.js: can now set rules externally

--- life.js
+++ life.js
@@ -1,55 +1,58 @@
-'use strict';
+window.$_life = function(liveRangeLow, liveRangeHigh, birthNum) {
 
-const _Rand = Math.random;
-const _Round = Math.round;
+  'use strict';
 
-const mappable = (len) => {
-  let arr = [];
-  for(let i = 0; i < len; i++) {
-    arr.push(0);
-  }
-  return arr;
-};
+  const _Rand = Math.random;
+  const _Round = Math.round;
 
-const populate = (el) => _Round(_Rand());
+  const mappable = (len) => {
+    let arr = [];
+    for(let i = 0; i < len; i++) {
+      arr.push(0);
+    }
+    return arr;
+  };
 
-const liveOrDie = (y, x, z, state) => {
+  const populate = (el) => _Round(_Rand());
 
-  const walk = (cb) => {
-    for(let y = -1; y <= 1; y++) {
-      for(let x = -1; x <= 1; x++) {
-        for(let z = -1; z <= 1; z++) {
-          if(y === 0 && x === 0 && z === 0) continue;
-          cb(y, x, z);
+  const liveOrDie = (y, x, z, state) => {
+
+    const walk = (cb) => {
+      for(let y = -1; y <= 1; y++) {
+        for(let x = -1; x <= 1; x++) {
+          for(let z = -1; z <= 1; z++) {
+            if(y === 0 && x === 0 && z === 0) continue;
+            cb(y, x, z);
+          }
         }
       }
+    };
+
+    const grab = (arr, y, x, z) => {
+      if(!arr[y]) return 0;
+      if(!arr[y][x]) return 0;
+      return arr[y][x][z] || 0;
+    };
+
+    let thisCellIsAlive = state[y][x][z];
+
+    let liveNeighbors = 0;
+    walk(function(offsetY, offsetX, offsetZ) {
+      if(grab(state, y+offsetY, x+offsetX, z+offsetZ)) liveNeighbors++;
+    });
+
+    if(thisCellIsAlive) {
+      return (liveNeighbors >= liveRangeLow && liveNeighbors <= liveRangeHigh) ? 1 : 0;
+    } else {
+      return (liveNeighbors === birthNum) ? 1 : 0;
     }
   };
 
-  const grab = (arr, y, x, z) => {
-    if(!arr[y]) return 0;
-    if(!arr[y][x]) return 0;
-    return arr[y][x][z] || 0;
-  };
-
-  let thisCellIsAlive = state[y][x][z];
-
-  let liveNeighbors = 0;
-  walk(function(offsetY, offsetX, offsetZ) {
-    if(grab(state, y+offsetY, x+offsetX, z+offsetZ)) liveNeighbors++;
-  });
-
-  if(thisCellIsAlive) {
-    return (liveNeighbors === 2 || liveNeighbors === 3) ? 1 : 0;
-  } else {
-    return (liveNeighbors === 3) ? 1 : 0;
-  }
-};
-
-
-window.$_life = function() {
-
   return {
+
+    setLiveRangeLow(num) { return liveRangeLow = num; },
+    setLiveRangeHigh(num) { return liveRangeHigh = num; },
+    setBirthNum(num) { return birthNum = num; },
 
     state: [],
 
