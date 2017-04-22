38b283ff4b8ce847f8438d0527a5a0a95787d5f1
iAmNathanJ <jacobs@heysparkbox.com>
2017-04-21 21:01:55 -0400

    small refactor

--- src/js/life.js
+++ src/js/life.js
@@ -1,7 +1,5 @@
 window.$_life = function(liveRangeLow, liveRangeHigh, birthNum) {
 
-  'use strict';
-
   const _Rand = Math.random;
   const _Round = Math.round;
 
@@ -15,29 +13,29 @@ window.$_life = function(liveRangeLow, liveRangeHigh, birthNum) {
 
   const populate = (el) => _Round(_Rand());
 
-  const liveOrDie = (y, x, z, state) => {
-
-    const walk = (cb) => {
-      for(let y = -1; y <= 1; y++) {
-        for(let x = -1; x <= 1; x++) {
-          for(let z = -1; z <= 1; z++) {
-            if(y === 0 && x === 0 && z === 0) continue;
-            cb(y, x, z);
-          }
+  const walk = (y, x, z) => (cb) => {
+    for(let y = -1; y <= 1; y++) {
+      for(let x = -1; x <= 1; x++) {
+        for(let z = -1; z <= 1; z++) {
+          if(y === 0 && x === 0 && z === 0) continue;
+          cb(y, x, z);
         }
       }
-    };
+    }
+  };
 
-    const grab = (arr, y, x, z) => {
-      if(!arr[y]) return 0;
-      if(!arr[y][x]) return 0;
-      return arr[y][x][z] || 0;
-    };
+  const grab = (arr, y, x, z) => {
+    if(!arr[y]) return 0;
+    if(!arr[y][x]) return 0;
+    return arr[y][x][z] || 0;
+  };
+
+  const liveOrDie = (y, x, z, state) => {
 
     let thisCellIsAlive = state[y][x][z];
 
     let liveNeighbors = 0;
-    walk(function(offsetY, offsetX, offsetZ) {
+    walk(y, x, z)((offsetY, offsetX, offsetZ) => {
       if(grab(state, y+offsetY, x+offsetX, z+offsetZ)) liveNeighbors++;
     });
 
